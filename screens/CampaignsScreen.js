f5e80bc07fc8875c10cf2a5714eb7cffd009fb8d
Anshul Mehta <anshul.mk97@gmail.com>
2017-10-02 17:17:54 +0530

    All Companies Screen

--- screens/CampaignsScreen.js
+++ screens/CampaignsScreen.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Container, Header, Content, List, ListItem, Thumbnail, Text, Body } from 'native-base';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image, View } from 'react-native';
 export default class CampaignsScreen extends Component {
   
   static navigationOptions = {
     title: 'Companies',
+    headerLeft: null,
+    headerTitleStyle: { alignSelf: 'center' },
   };
 
   
@@ -13,12 +15,12 @@ export default class CampaignsScreen extends Component {
     
     const { navigate } = this.props.navigation;
 
-    var camp = [    {"name":"Anshul Mehta","details":"Anshul Mehta 123"},
-                    {"name":"Alok Singh","details":"Anshul 123"},
-                    {"name":"Anirudh Khandelwal","details":"Anirudh 123"},
-                    {"name":"Amazon","details":"Amazon 123"},
-                    {"name":"Flipkart","details":"Anirudh 123"},
-                    {"name":"Snapdeal","details":"Snapdeal 123"}
+    var camp = [    {"name":"Amazon","type":"Wholesale","rating":"4.6","enrolled":"933"},
+                    {"name":"Tesla","type":"Cars","rating":"4.1","enrolled":"522"},
+                    {"name":"SpaceX","type":"Rockets","rating":"4.8","enrolled":"296"},
+                    {"name":"Adidas","type":"Retail","rating":"5.0","enrolled":"844"},
+                    {"name":"Flipkart","type":"Ecommerce","rating":"3.9","enrolled":"326"},
+                    {"name":"Snapdeal","type":"Ecommerce","rating":"4.3","enrolled":"147"}
                 ];
     return (
       <Container style={styles.container} >
@@ -28,8 +30,14 @@ export default class CampaignsScreen extends Component {
             <ListItem onPress={() => navigate('CampaignsDetScreen', { name: `${camp.name}`})}>
               <Image style={styles.thumbnailStyle} source={{ uri: 'http://media.corporate-ir.net/media_files/IROL/17/176060/img/logos/amazon_logo_RGB.jpg' }} />
               <Body>
-                <Text>{camp.name}</Text>
-                <Text note>{camp.details}</Text>
+                <View style={styles.viewTextStyle}>
+                  <Text>{camp.name}</Text>
+                  <Text style={styles.ratingTextStyle} note>{camp.rating}/5.0</Text>
+                </View>
+                <View style={styles.viewTextStyle}>
+                  <Text note>{camp.type}</Text>
+                  <Text note>{camp.enrolled} Enrolled</Text>
+                </View>
               </Body>
             </ListItem>
           }>
@@ -50,5 +58,12 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     height: 60,
     width: 60,
+  },
+  ratingTextStyle: {
+    textAlign: 'right'
+  },
+  viewTextStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
