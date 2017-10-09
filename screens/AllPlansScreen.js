2b4c682915175628b30368ecd0d3c0a87d60e8a8
Alok Singh <singhalok641@gmail.com>
2017-10-09 19:36:19 +0530

    fixed like icon

--- screens/AllPlansScreen.js
+++ screens/AllPlansScreen.js
@@ -46,34 +46,34 @@ export default class CampaignsScreen extends Component {
     
     const { navigate } = this.props.navigation;
 
-    var plans = [    {"name":"Amazon A","difficulty":"Easy",likes:"20",price:"200"},
-                    {"name":"Snapdeal B","difficulty":"Easy",likes:"63",price:"350"},
-                    {"name":"Flipkart A","difficulty":"Medium",likes:"36",price:"400"},
-                    {"name":"Instamojo A","difficulty":"Easy",likes:"2",price:"2000"},
-                    {"name":"Thyrocare C","difficulty":"Hard",likes:"59",price:"2400"},
-                    {"name":"MakeMyTrips B","difficulty":"Easy",likes:"12",price:"1200"},
+    var plans = [    {"id":"1","name":"Amazon A","difficulty":"Easy",likes:"20",price:"200"},
+                    {"id":"2","name":"Snapdeal B","difficulty":"Easy",likes:"63",price:"350"},
+                    {"id":"4","name":"Instamojo A","difficulty":"Easy",likes:"2",price:"2000"},
+                    {"id":"5","name":"Thyrocare C","difficulty":"Hard",likes:"59",price:"2400"},
+                    {"id":"6","name":"MakeMyTrips B","difficulty":"Easy",likes:"12",price:"1200"},
                 ];
     return (
       <Container style={styles.container} >
         <Content>
-
-        
-
           <List dataArray={plans}
             renderRow={(plan) =>
-            <ListItem onPress={() => navigate('PlansScreen')}>
+            <ListItem onPress={() => navigate('PlansScreen', { id: `${plan.id}`, name: `${plan.name}`})}>
               <Image style={styles.thumbnailStyle} source={{ uri: plan.logo }} />
               <Body>
                 <View style={styles.viewTextStyle}>
                   <Text>{plan.name}</Text>
-                  <Text note>Rs {plan.price_of_product}</Text>
+                  <Text note>Rs {plan.price}</Text>
                 </View>
                 <View style={styles.viewTextStyle}>
-                  <Text note>{plan.difficulty}</Text>
-                  <Text note> 
-                    <EvilIcons name="like" size={18} color="#000000" />
-                    {plan.likes}
-                  </Text>
+                  <Text note >{plan.difficulty}</Text>
+                  <View style={{ left:110 }}>
+                    <EvilIcons name="like" size={30} color="#000000" />
+                  </View>
+                  <View style={{ top:5 }}>
+                    <Text note> 
+                      {plan.likes}
+                    </Text>
+                  </View>
                 </View>
               </Body>
             </ListItem>
